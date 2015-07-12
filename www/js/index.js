c53ad118776a30337ff00031c6408df6e4dfe2af
missoby <missoby@hotmail.fr>
2015-07-12 17:26:04 +0100

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -10,25 +10,35 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     pictureSource = navigator.camera.PictureSourceType;
     destinationType = navigator.camera.DestinationType;
+    alert(destinationType);
+    alert("fff");
 }
 function onPhotoDataSuccess(imageData) {
+    alert("onPhotoDataSuccess avant");
     var smallImage = document.getElementById('smallImage');
     smallImage.style.display = 'block';
     smallImage.src = "data:image/jpeg;base64," + imageData;
+    alert("onPhotoDataSuccess après");
+
+
 }
 
 // Called when a photo is successfully retrieved
 function onPhotoURISuccess(imageURI) {
+    alert("onPhotoURISuccess");
     var smallImage = document.getElementById('smallImage');
     smallImage.style.display = 'block';
     smallImage.src = imageURI;
+    alert("onPhotoURISuccess après");
 }
 // A button will call this function
 //
 function capturePhoto() {
+    alert("capture photos 1");
     // Take picture using device camera and retrieve image as base64-encoded string
     navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50,
         destinationType: destinationType.DATA_URL});
+    alert("capture photos");
 }
 
 // A button will call this function
@@ -53,3 +63,5 @@ function getPhoto(source) {
 function onFail(message) {
     alert('Failed because: ' + message);
 }
+
+alert("fin script");
